576a9ec7a066e44f9f5f481b0732240bc89c6297
CianTivnan <cian.tivnan@yahoo.ie>
2023-02-26 18:56:30 +0000

    Moved to research project, keyboard & hand tracking implemented.
    Moved to research project, keyboard & hand tracking implemented.

--- Assets/Oculus/SampleFramework/Core/DebugUI/Scripts/HandedInputSelector.cs
+++ Assets/Oculus/SampleFramework/Core/DebugUI/Scripts/HandedInputSelector.cs
@@ -50,15 +50,5 @@ public class HandedInputSelector : MonoBehaviour
 
     void SetActiveController(OVRInput.Controller c)
     {
-        Transform t;
-        if(c == OVRInput.Controller.LTouch)
-        {
-            t = m_CameraRig.leftHandAnchor;
-        }
-        else
-        {
-            t = m_CameraRig.rightHandAnchor;
-        }
-        m_InputModule.rayTransform = t;
     }
 }
