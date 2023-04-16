3fba917940897fcc22cc565b2bb676e86ec07899
CianTivnan <cian.tivnan@yahoo.ie>
2023-04-16 17:30:05 +0100

    Update CustomControls.cs
    Adjusting custom control script to manually take keycode input

--- Assets/CustomControls.cs
+++ Assets/CustomControls.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
 
 public class CustomControls : MonoBehaviour
 {
@@ -59,16 +60,88 @@ public class CustomControls : MonoBehaviour
     void OnGUI()
     {
         //string key = "";
-        //bool keydown = false;
-        //Event e = Event.current;
-        //if (e.type.Equals(EventType.KeyDown) && !keydown)
-        //{
-        //    keydown = true;
-        //    key = e.keyCode.ToString();
-        //    StartingFocusField.text = StartingFocusField.text + key;
-        //}
+        bool keydown = false;
+        Event e = Event.current;
+        if (e.type.Equals(EventType.KeyDown) && !keydown)
+        {
+            keydown = true;
 
-        //if (e.type.Equals(EventType.KeyUp))
-        //    keydown = false;
+            if (e.keyCode == KeyCode.Backspace)
+            {
+                if (StartingFocusField.text.Length > 0)
+                {
+                    // Remove the last character from the text
+                    StartingFocusField.text = StartingFocusField.text.Substring(0, StartingFocusField.text.Length - 1);
+                }
+            }
+            else
+            {
+                // Get the output of the key pressed
+                string output = GetKeyOutput(e);
+
+                if (output != "")
+                {
+                    // Add the output to the text
+                    StartingFocusField.text += output;
+                }
+            }           
+            //StartingFocusField.text = StartingFocusField.text + GetKeyOutput(Event.current);
+            //key = e.keyCode.ToString();
+            //StartingFocusField.text = StartingFocusField.text + key;
+        }
+
+        if (e.type.Equals(EventType.KeyUp))
+            keydown = false;
+    }
+
+    string GetKeyOutput(Event e)
+    {
+        // Check if Shift key is held down
+        bool isShiftPressed = (Event.current.modifiers & EventModifiers.Shift) != 0;
+
+        // Check if KeyCode is alphabetical
+        if (e.type == EventType.KeyDown && e.keyCode >= KeyCode.A && e.keyCode <= KeyCode.Z)
+        {
+            // Convert KeyCode to char
+            char c = (char)((int)e.keyCode + (isShiftPressed ? 'A' - 'a' : 0));
+            return c.ToString();
+        }
+
+        // Check if KeyCode is a number
+        if (e.type == EventType.KeyDown && e.keyCode >= KeyCode.Alpha0 && e.keyCode <= KeyCode.Alpha9)
+        {
+            // Convert KeyCode to char
+            char c = (char)((int)e.keyCode - KeyCode.Alpha0 + '0');
+            return c.ToString();
+        }
+
+        // Check if KeyCode is a symbol
+        switch (e.keyCode)
+        {
+            case KeyCode.Space:
+                return " ";
+            case KeyCode.Comma:
+                return isShiftPressed ? "<" : ",";
+            case KeyCode.Period:
+                return isShiftPressed ? ">" : ".";
+            case KeyCode.Semicolon:
+                return isShiftPressed ? ":" : ";";
+            case KeyCode.Quote:
+                return isShiftPressed ? "\"" : "'";
+            case KeyCode.Minus:
+                return isShiftPressed ? "_" : "-";
+            case KeyCode.Equals:
+                return isShiftPressed ? "+" : "=";
+            case KeyCode.LeftBracket:
+                return isShiftPressed ? "{" : "[";
+            case KeyCode.RightBracket:
+                return isShiftPressed ? "}" : "]";
+            case KeyCode.Backslash:
+                return isShiftPressed ? "|" : "\\";
+            case KeyCode.Slash:
+                return isShiftPressed ? "?" : "/";
+            default:
+                return "";
+        }
     }
 }
