bbc19640b0aa7d0c1e0de1553b198842db3ebfda
CianTivnan <cian.tivnan@yahoo.ie>
2023-04-16 20:41:19 +0100

    Keyboard input working fully

--- Assets/CustomControls.cs
+++ Assets/CustomControls.cs
@@ -10,14 +10,15 @@ public class CustomControls : MonoBehaviour
     public OVRTrackedKeyboard trackedKeyboard;
     public InputField StartingFocusField;
     public OVRInput.RawButton keyboardButton;
+    bool isCapsLockOn = false; // Initialize caps lock state
 
 
     private void OnEnable()
     {
-        EventSystem.current.SetSelectedGameObject(StartingFocusField.gameObject, null);
-        StartingFocusField.OnPointerClick(new PointerEventData(EventSystem.current));
-        StartingFocusField.ActivateInputField();
-        StartCoroutine(SelectInputField());
+        //EventSystem.current.SetSelectedGameObject(StartingFocusField.gameObject, null);
+        //StartingFocusField.OnPointerClick(new PointerEventData(EventSystem.current));
+        //StartingFocusField.ActivateInputField();
+        //StartCoroutine(SelectInputField());
     }
 
     IEnumerator SelectInputField()
@@ -40,20 +41,20 @@ public class CustomControls : MonoBehaviour
 
     private void Update()
     {
-        // Check if an external keyboard is connected
-        if (trackedKeyboard.ConnectionRequired)
-        {
-            // Enable the input field for external keyboard input
-            StartingFocusField.interactable = true;
-            StartingFocusField.ActivateInputField();
-            StartingFocusField.shouldHideMobileInput = true;
-        }
-        else
-        {
-            // Disable the input field to prevent the onboard virtual keyboard from appearing
-            StartingFocusField.interactable = false;
-            StartingFocusField.DeactivateInputField();
-        }
+        //// Check if an external keyboard is connected
+        //if (trackedKeyboard.ConnectionRequired)
+        //{
+        //    // Enable the input field for external keyboard input
+        //    StartingFocusField.interactable = true;
+        //    StartingFocusField.ActivateInputField();
+        //    StartingFocusField.shouldHideMobileInput = true;
+        //}
+        //else
+        //{
+        //    // Disable the input field to prevent the onboard virtual keyboard from appearing
+        //    StartingFocusField.interactable = false;
+        //    StartingFocusField.DeactivateInputField();
+        //}
 
     }
 
@@ -66,7 +67,12 @@ public class CustomControls : MonoBehaviour
         {
             keydown = true;
 
-            if (e.keyCode == KeyCode.Backspace)
+            if (e.keyCode == KeyCode.CapsLock)
+            {
+                // Toggle caps lock state
+                isCapsLockOn = !isCapsLockOn;
+            }
+            else if (e.keyCode == KeyCode.Backspace)
             {
                 if (StartingFocusField.text.Length > 0)
                 {
@@ -74,30 +80,35 @@ public class CustomControls : MonoBehaviour
                     StartingFocusField.text = StartingFocusField.text.Substring(0, StartingFocusField.text.Length - 1);
                 }
             }
+            else if (e.keyCode == KeyCode.Return)
+            {
+                // Append a newline character to the text
+                StartingFocusField.text += "\n";
+            }
             else
             {
                 // Get the output of the key pressed
-                string output = GetKeyOutput(e);
+                string output = GetKeyOutput(e, isCapsLockOn);
 
                 if (output != "")
                 {
                     // Add the output to the text
                     StartingFocusField.text += output;
                 }
-            }           
-            //StartingFocusField.text = StartingFocusField.text + GetKeyOutput(Event.current);
-            //key = e.keyCode.ToString();
-            //StartingFocusField.text = StartingFocusField.text + key;
+            }
         }
+        //StartingFocusField.text = StartingFocusField.text + GetKeyOutput(Event.current);
+        //key = e.keyCode.ToString();
+        //StartingFocusField.text = StartingFocusField.text + key;
 
         if (e.type.Equals(EventType.KeyUp))
             keydown = false;
     }
 
-    string GetKeyOutput(Event e)
+    string GetKeyOutput(Event e, bool isCapsLockOn)
     {
-        // Check if Shift key is held down
-        bool isShiftPressed = (Event.current.modifiers & EventModifiers.Shift) != 0;
+        // Check if Shift key or caps lock is held down
+        bool isShiftPressed = (Event.current.modifiers & EventModifiers.Shift) != 0 || isCapsLockOn;
 
         // Check if KeyCode is alphabetical
         if (e.type == EventType.KeyDown && e.keyCode >= KeyCode.A && e.keyCode <= KeyCode.Z)
@@ -111,8 +122,39 @@ public class CustomControls : MonoBehaviour
         if (e.type == EventType.KeyDown && e.keyCode >= KeyCode.Alpha0 && e.keyCode <= KeyCode.Alpha9)
         {
             // Convert KeyCode to char
-            char c = (char)((int)e.keyCode - KeyCode.Alpha0 + '0');
-            return c.ToString();
+            if (isShiftPressed)
+            {
+                switch (e.keyCode)
+                {
+                    case KeyCode.Alpha1:
+                        return "!";
+                    case KeyCode.Alpha2:
+                        return "@";
+                    case KeyCode.Alpha3:
+                        return "#";
+                    case KeyCode.Alpha4:
+                        return "$";
+                    case KeyCode.Alpha5:
+                        return "%";
+                    case KeyCode.Alpha6:
+                        return "^";
+                    case KeyCode.Alpha7:
+                        return "&";
+                    case KeyCode.Alpha8:
+                        return "*";
+                    case KeyCode.Alpha9:
+                        return "(";
+                    case KeyCode.Alpha0:
+                        return ")";
+                    default:
+                        return "";
+                }
+            }
+            else
+            {
+                char c = (char)((int)e.keyCode - KeyCode.Alpha0 + '0');
+                return c.ToString();
+            }
         }
 
         // Check if KeyCode is a symbol
